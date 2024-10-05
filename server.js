59ad3ae19595f7027573af0393351c2675f9f726
FelixAgene <agene001@umn.edu>
2024-10-04 21:26:45 -0500

    new commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080; // Use the PORT from environment or default to 8080
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0",() => {
     console.log(`Example app listening on port ${port}`)
 })
