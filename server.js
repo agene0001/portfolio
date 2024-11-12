bb0be4aa12756bdcc3c0ebf3c23bb4a8e55108e5
FelixAgene <agene001@umn.edu>
2024-11-12 03:15:22 -0600

    new commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8081; // Use the PORT from environment or default to 8080
+// port = 8088
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
